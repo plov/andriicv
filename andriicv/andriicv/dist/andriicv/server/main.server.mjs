14acebe39d48dd3f2ac869f519259bccff7106a3
Andriі Pavlov <plov.mail@gmail.com>
2024-06-24 20:28:08 -0500

    Main (#5)
    * node 18.18.2
    
    * package lock json updated
    
    * update contacts
    
    * modified:   andriicv/.gitignore
    
    * changes output path

--- andriicv/andriicv/dist/andriicv/server/main.server.mjs
+++ andriicv/andriicv/dist/andriicv/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AOQWFNSE.mjs";import"./chunk-HSFJXEZN.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
+import{a}from"./chunk-3ISXNY6R.mjs";import"./chunk-HSFJXEZN.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
