9c6fe7481146cefc45c87dfd469176993f86e1e0
Andriі Pavlov <plov.mail@gmail.com>
2024-07-03 16:40:23 -0500

    ssr turned off

--- andriicv/src/app/app.routes.ts
+++ andriicv/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginformComponent } from './loginform/loginform.component';
 import { HeaderComponent } from './header/header.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
+    {path: 'app-root', component: AppComponent}, 
     {path: 'app-loginform', component: LoginformComponent}, 
     {path: 'app-header', component: HeaderComponent},
 ];
