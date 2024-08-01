6d370ab481367bb83056bbab660075b3926b2091
Andriі Pavlov <plov.mail@gmail.com>
2024-07-31 21:23:03 -0500

    try use aws envarenment

---
+++ andriicv/set-environment.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+// List of environment variables that you want to replace
+const envVariables = ['ACCESS_KEY', 'SECRET_KEY'];
+const templateFilePath = path.join(__dirname, 'src/environments/environment.prod.template.ts');
+const targetFilePath = path.join(__dirname, 'src/environments/environment.prod.ts');
+
+let content = fs.readFileSync(templateFilePath, 'utf-8');
+
+envVariables.forEach((variable) => {
+    const value = process.env[variable];
+    if (value === undefined) {
+      console.warn(`Warning: Environment variable ${variable} is not defined.`);
+    }
+    const replacement = value ?? '';
+    const placeholder = new RegExp(`\\$\\{${variable}\\}`, 'g');
+    content = content.replace(placeholder, replacement);
+  });
+  
+
+// Write the final content to environment.prod.ts
+fs.writeFileSync(targetFilePath, content);
+
+console.log(`Environment variables set in ${targetFilePath}`);
---
+++ andriicv/src/environments/environment.prod.template.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    accessKeyId: '${ACCESS_KEY}',
+    secretAccessKey: '${SECRET_KEY}'
+  };
