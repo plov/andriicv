0e799f002f02df28087c57b621d08409937fd0e1
Andriі Pavlov <plov.mail@gmail.com>
2024-09-27 19:43:08 -0500

    fix 2

--- andriicv/src/environments/environment.prod.template.ts
+++ andriicv/src/environments/environment.prod.template.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     accessKeyId: '${ACCESS_KEY}',
-    secretAccessKey: '${SECRET_KEY}'
+    secretAccessKey: '${SECRET_KEY}',
+    apiUrl: 'https://rdgyvzynic.execute-api.us-east-1.amazonaws.com/PlovProd/'
   };
